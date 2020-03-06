0e3b5174eafacc4ff1295795ba2857634721fbfd
Paritosh Batish <batishparitosh2@gmail.com>
2020-03-07 01:03:46 +0530

    no

--- routes/home.js
+++ routes/home.js
@@ -38,10 +38,7 @@ router.post("/login", async (req, res) => {
     name: req.body.username,
     password: req.body.password
   });
-  if (result[0]) {
-    result[0].status = true;
-    return res.status(200).send(result[0]);
-  }
+  if (result[0]) return res.status(200).send(result[0]);
   else return res.status(400).send({ status: false });
 });
 
