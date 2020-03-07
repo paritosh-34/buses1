db7e043546ed1bc5e856d7878cae70bc72998cf2
Paritosh Batish <batishparitosh2@gmail.com>
2020-03-08 04:05:43 +0530

    removed bugs in api's

--- routes/home.js
+++ routes/home.js
@@ -9,6 +9,39 @@ const { Transaction } = require("../models/transactions");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  if (req.session._id) {
+    if (req.session._id === "5e63adda4f8fb2266eb514eb") {
+      return res.redirect("/admin");
+    }
+    return res.redirect("/mappage");
+  }
+  return res.render("home.html");
+});
+
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const result = await User.find({
+    name: req.body.username,
+    password: req.body.password
+  });
+  if (result[0]) {
+    console.log(result[0]);
+    req.session._id = result[0]._id;
+    return res.redirect("/mappage");
+  } else return res.render("home.html", { i: "Invalid id/password" });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(function(err) {
+    if (err) {
+      return res.negotiate(err);
+    }
+    return res.redirect("/");
+  });
+});
+
 router.get("/admin", async (req, res) => {
   if (req.session._id === "5e63adda4f8fb2266eb514eb") {
     const locations = await Location.find()
@@ -40,18 +73,6 @@ router.get("/mappage", async (req, res) => {
   return res.redirect("/");
 });
 
-router.post("/", async (req, res) => {
-  console.log(req.body);
-  const result = await User.find({
-    name: req.body.username,
-    password: req.body.password
-  });
-  if (result[0]) {
-    console.log(result[0]);
-    req.session._id = result[0]._id;
-    return res.redirect("/mappage");
-  } else return res.render("home.html", { i: "Invalid id/password" });
-});
 
 router.post("/login", async (req, res) => {
   console.log(req.body);
@@ -102,21 +123,6 @@ router.get("/map", (req, res) => {
   res.render("maps.html");
 });
 
-router.get("/testL/MH%2014%20GU%207964", async (req, res) => {
-  const result = await Location.find({ name: "MH 14 GU 7964" }).sort("-time");
-  res.send(result);
-});
-
-router.get("/testL/Akash", async (req, res) => {
-  const result = await Location.find({ name: "Akash" }).sort("-time");
-  res.send(result);
-});
-
-router.get("/testL/Nakul", async (req, res) => {
-  const result = await Location.find({ name: "Nakul" }).sort("-time");
-  res.send(result);
-});
-
 router.get("/test", (req, res) => {
   res.render("test_socket.html");
 });
@@ -129,23 +135,5 @@ router.get("/map2", (req, res) => {
   res.render("maps2.html");
 });
 
-router.get("/", (req, res) => {
-  if (req.session._id) {
-    if (req.session._id === "5e63adda4f8fb2266eb514eb") {
-      return res.redirect("/admin");
-    }
-    return res.redirect("/mappage");
-  }
-  return res.render("home.html");
-});
-
-router.get("/logout", (req, res) => {
-  req.session.destroy(function(err) {
-    if (err) {
-      return res.negotiate(err);
-    }
-    return res.redirect("/");
-  });
-});
 
 module.exports = router;
